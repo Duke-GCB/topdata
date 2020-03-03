4512cc964495a7e43094dd1c21177e300c7c0695
John Bradley <johnbradley2008@gmail.com>
2020-03-03 16:06:42 -0500

    rough out new workflow for testing
    Breaks out UI into three steps. User picks transcription factors,
    user picks cell types, user chooses tracks that are for the previously
    selected tf/cell type. User is taken to genome browser.

--- tracks/urls.py
+++ tracks/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='tracks-index'),
+    path('select-factors/', views.select_factors, name='tracks-select_factors'),
+    path('select-cell-type/', views.select_cell_type, name='tracks-select_cell_type'),
+    path('choose-combinations/', views.choose_combinations, name='tracks-choose_combinations'),
+    path('view-genome-browser/', views.view_genome_browser, name='tracks-view_genome_browser'),
     path('<encoded_key_value>/', views.detail, name='tracks-detail'),
     path('<encoded_key_value>/hub.txt', views.hub, name='tracks-hub'),
     path('<encoded_key_value>/genomes.txt', views.genomes, name='tracks-genomes'),
     path('<encoded_key_value>/<genome>/trackDb.txt', views.track_db, name='tracks-trackdb'),
-
 ]
